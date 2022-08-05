b8d2e6d08fdb3ea1e4f6e7d0c0a6483657ac4994
superbetty2005 <98929332+superbetty2005@users.noreply.github.com>
2022-08-04 23:47:07 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 #  terraform {
 #    backend "s3" {
-#     bucket = "avinash-terraform-state"
+#     bucket = "beatty-07-30-2022-terraform-state"
 #     key    = "state.tfstate"
-#     region = "us-west-2"
+#     region = "us-east-1"
 #     profile = "jespo"
 # dynamodb_table= "firstprojecttable"
 #    }
